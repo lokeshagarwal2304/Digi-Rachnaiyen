6cd5d7d39b5c092aca11dc683812d7d40d4da6b2
lokeshagarwal2304 <lokeshagarwal2304@gmail.com>
2024-11-18 18:01:52 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,77 @@
+/*Title change*/
+const titles = ["Developer.👨‍💻", "Graphic Designer.🎨", "AI Aspirant.😃"];
+let index = 0;
+
+function changeTitle() {
+    const titleElement = document.getElementById('dynamic-title');
+    titleElement.classList.add('hidden'); // Fade out
+
+    setTimeout(() => {
+        titleElement.textContent = titles[index]; // Change title
+        titleElement.classList.remove('hidden'); // Fade in
+
+        index = (index + 1) % titles.length; // Cycle through titles
+    }, 500); // Wait for fade out to finish
+
+    setTimeout(changeTitle, 2000); // Change title every 2 seconds
+}
+
+// Start the title changing effect
+changeTitle();
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
